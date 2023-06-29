b31bc1fc61fc5d7298eea90a29e459b187d6a3dc
MotaGuilherme <metatronmota@gmail.com>
2023-06-29 01:44:52 -0300

    Testing first class of domain - Category

---
+++ src/main/java/org/example/domain/Category.java
@@ -0,0 +1,50 @@
+package org.example.domain;
+
+import java.io.Serializable;
+
+public class Category implements Serializable {
+
+    private Integer id;
+    private String name;
+
+    public Category() {
+
+    }
+
+    public Category(Integer id, String name) {
+        super();
+        this.id = id;
+        this.name = name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Category category = (Category) o;
+
+        return id.equals(category.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return id.hashCode();
+    }
+}
--- src/main/java/org/example/resources/CategoryResource.java
+++ src/main/java/org/example/resources/CategoryResource.java
@@ -1,8 +1,12 @@
 package org.example.resources;
 
+import org.example.domain.Category;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 @RestController
@@ -10,7 +14,15 @@ import org.springframework.web.bind.annotation.RestController;
 public class CategoryResource {
 
     @RequestMapping(method = RequestMethod.GET)
-    public String list() {
-        return "REST está funcionando";
+    public List<Category> list() {
+
+        Category cat1 = new Category(1, "informatica");
+        Category cat2 = new Category(2, "escritorio");
+
+        List<Category> list = new ArrayList<>();
+        list.add(cat1);
+        list.add(cat2);
+
+        return list;
     }
 }
