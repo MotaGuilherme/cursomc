2cc2793a0c6d7624266c48ce9c4c77e2b62bfc34
MotaGuilherme <metatronmota@gmail.com>
2023-06-29 14:42:15 -0300

    adding H2 database and creation of automatic data base

--- src/main/java/org/example/domain/Category.java
+++ src/main/java/org/example/domain/Category.java
@@ -1,9 +1,17 @@
 package org.example.domain;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
 import java.io.Serializable;
 
+@Entity
 public class Category implements Serializable {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String name;
 
