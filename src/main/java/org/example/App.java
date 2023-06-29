de97b33dc717a8a20f351f62afaa3f4a412aca9a
MotaGuilherme <metatronmota@gmail.com>
2023-06-29 16:33:40 -0300

    implements create instanciation

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,16 +1,30 @@
 package org.example;
 
+import org.example.domain.Category;
+import org.example.repositories.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-/**
- * Hello world!
- *
- */
+import java.util.Arrays;
+
+
 @SpringBootApplication
-public class App 
-{
+public class App  implements CommandLineRunner {
+
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+
     public static void main( String[] args ) {
         SpringApplication.run(App.class, args);
+    }
+
+    public void run(String ...args) throws Exception {
+        Category cat1 = new Category(null, "Informática");
+        Category cat2 = new Category(null, "Escritório");
+
+        categoryRepository.saveAll(Arrays.asList(cat1, cat2));
     }
 }
